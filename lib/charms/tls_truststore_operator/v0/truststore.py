2ae587387376b413bf11fbc49cdd42110fd68b22
Pietro Pasotti <starfire.daemon@gmail.com>
2023-06-07 11:50:04 +0200

    stripped generate csr code

--- lib/charms/tls_truststore_operator/v0/truststore.py
+++ lib/charms/tls_truststore_operator/v0/truststore.py
@@ -1,14 +1,9 @@
 """
 """
 import json
-import uuid
-from ipaddress import IPv4Address
-from typing import Dict, List, Optional
+from typing import Dict, List
 
 import pydantic as pydantic
-from cryptography import x509
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
 from ops import Unit, Model, Relation
 from pydantic import Json
 
@@ -35,107 +30,6 @@ class TrustStoreProviderAppDatabagSchema(pydantic.BaseModel):
     certificate: str
     # a mapping from unit names to Trust objects
     trust: Json[Dict[str, Trust]]
-
-
-def generate_private_key(
-        password: Optional[bytes] = None,
-        key_size: int = 2048,
-        public_exponent: int = 65537,
-) -> bytes:
-    """Generates a private key.
-
-    Args:
-        password (bytes): Password for decrypting the private key
-        key_size (int): Key size in bytes
-        public_exponent: Public exponent.
-
-    Returns:
-        bytes: Private Key
-    """
-    private_key = rsa.generate_private_key(
-        public_exponent=public_exponent,
-        key_size=key_size,
-    )
-    key_bytes = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.BestAvailableEncryption(password)
-        if password
-        else serialization.NoEncryption(),
-    )
-    return key_bytes
-
-
-def generate_csr(
-        private_key: bytes,
-        subject: str,
-        add_unique_id_to_subject_name: bool = True,
-        organization: Optional[str] = None,
-        email_address: Optional[str] = None,
-        country_name: Optional[str] = None,
-        private_key_password: Optional[bytes] = None,
-        sans: Optional[List[str]] = None,
-        sans_oid: Optional[List[str]] = None,
-        sans_ip: Optional[List[str]] = None,
-        sans_dns: Optional[List[str]] = None,
-        additional_critical_extensions: Optional[List] = None,
-) -> bytes:
-    """Generates a CSR using private key and subject.
-
-    Args:
-        private_key (bytes): Private key
-        subject (str): CSR Subject.
-        add_unique_id_to_subject_name (bool): Whether a unique ID must be added to the CSR's
-            subject name. Always leave to "True" when the CSR is used to request certificates
-            using the tls-certificates relation.
-        organization (str): Name of organization.
-        email_address (str): Email address.
-        country_name (str): Country Name.
-        private_key_password (bytes): Private key password
-        sans (list): Use sans_dns - this will be deprecated in a future release
-            List of DNS subject alternative names (keeping it for now for backward compatibility)
-        sans_oid (list): List of registered ID SANs
-        sans_dns (list): List of DNS subject alternative names (similar to the arg: sans)
-        sans_ip (list): List of IP subject alternative names
-        additional_critical_extensions (list): List if critical additional extension objects.
-            Object must be a x509 ExtensionType.
-
-    Returns:
-        bytes: CSR
-    """
-    signing_key = serialization.load_pem_private_key(private_key, password=private_key_password)
-    subject_name = [x509.NameAttribute(x509.NameOID.COMMON_NAME, subject)]
-    if add_unique_id_to_subject_name:
-        unique_identifier = uuid.uuid4()
-        subject_name.append(
-            x509.NameAttribute(x509.NameOID.X500_UNIQUE_IDENTIFIER, str(unique_identifier))
-        )
-    if organization:
-        subject_name.append(x509.NameAttribute(x509.NameOID.ORGANIZATION_NAME, organization))
-    if email_address:
-        subject_name.append(x509.NameAttribute(x509.NameOID.EMAIL_ADDRESS, email_address))
-    if country_name:
-        subject_name.append(x509.NameAttribute(x509.NameOID.COUNTRY_NAME, country_name))
-    csr = x509.CertificateSigningRequestBuilder(subject_name=x509.Name(subject_name))
-
-    _sans: List[x509.GeneralName] = []
-    if sans_oid:
-        _sans.extend([x509.RegisteredID(x509.ObjectIdentifier(san)) for san in sans_oid])
-    if sans_ip:
-        _sans.extend([x509.IPAddress(IPv4Address(san)) for san in sans_ip])
-    if sans:
-        _sans.extend([x509.DNSName(san) for san in sans])
-    if sans_dns:
-        _sans.extend([x509.DNSName(san) for san in sans_dns])
-    if _sans:
-        csr = csr.add_extension(x509.SubjectAlternativeName(set(_sans)), critical=False)
-
-    if additional_critical_extensions:
-        for extension in additional_critical_extensions:
-            csr = csr.add_extension(extension, critical=True)
-
-    signed_certificate = csr.sign(signing_key, hashes.SHA256())  # type: ignore[arg-type]
-    return signed_certificate.public_bytes(serialization.Encoding.PEM)
 
 
 class TrustStoreProvider:
@@ -201,14 +95,5 @@ class TrustStoreRequirer:
             raise RuntimeError(f'too many or too few relations on {self._relation_name}')
         return rels[0]
 
-    @staticmethod
-    def generate_private_key(password: str):
-        return generate_private_key(password=password.encode('utf-8'))
-
-    def request_certificate(self, password: str, private_key: str, subject: str):
-        csr = generate_csr(
-            private_key=private_key.encode('utf-8'),
-            private_key_password=password.encode(),
-            subject=subject,
-        ).decode()
+    def request_certificate(self, csr: str):
         self.relation.data[self._unit]['certificate_signing_request'] = csr
